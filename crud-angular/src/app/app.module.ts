024c470aa285f6caae87e9db8ccb87669c738386
kayyra <anarodriguez.p@hotmail.es>
2023-06-20 11:10:20 +0200

    added img

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ HttpClientModule } from '@angular/common/http';
+import { ImgComponent } from './assets/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
